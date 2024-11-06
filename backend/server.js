515bfa6ec6da0af5199eb272103e6b7174110c1c
kayarhabby <pasciahershekaya@gmail.com>
2024-11-06 17:12:35 +0100

    fix server problem reload problem

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ var router_1 = require("./router");
 var PORT = 3000;
 var server = http.createServer(function (req, res) {
     // Middleware pour gérer CORS
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // Autorise uniquement le frontend
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-      res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // Autorise uniquement le frontend
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // Vérification si c'est une requête OPTIONS (pré-vol)
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204); // Pas de contenu, mais autorisé
+        res.end();
+        return;
+    }
     (0, router_1.default)(req, res);
 });
 server.listen(PORT, function () {
--- backend/server.ts
+++ backend/server.ts
@@ -3,18 +3,19 @@ import router from "./router";
 
 const PORT = 3000;
 
+
 const server = http.createServer((req, res) => {
-// // Middleware pour gérer CORS
-//   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // Autorise uniquement le frontend
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//
-//   // Vérification si c'est une requête OPTIONS (pré-vol)
-//   if (req.method === 'OPTIONS') {
-//     res.writeHead(204); // Pas de contenu, mais autorisé
-//     res.end();
-//     return;
-//   }
+// Middleware pour gérer CORS
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001'); // Autorise uniquement le frontend
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Vérification si c'est une requête OPTIONS (pré-vol)
+  if (req.method === 'OPTIONS') {
+    res.writeHead(204); // Pas de contenu, mais autorisé
+    res.end();
+    return;
+  }
   router(req, res);
 });
 
